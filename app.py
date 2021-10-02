d99f6cf970d1fb026ff8801894369af848c7ff07
namanmansukhani <naman.mansukh@gmail.com>
2021-10-01 20:50:48 -0400

    minor fixes

--- app.py
+++ app.py
@@ -2,24 +2,36 @@ from flask import Flask, make_response, request, jsonify
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def home():
+    return "hello"
+
+
 @app.route("/send", methods = ['GET'])
 def get_restaurant_info():
     if request.method != 'GET':
         return make_response('Malformed request', 400)
 
-    return make_response(
-        "Success",
-        200,
-        headers = headers
-    )
+    json_response = {
+        0: 1
+    }
+    return json_response
 
 
-@app.get("/submit", methods = ['POST'])
+@app.route("/submit", methods = ['POST'])
 def post_info():
     if request.method != 'POST':
         return make_response('Malformed request', 400)
+
+    return make_response(
+        "works",
+        200
+    )
     
 
 @app.errorhandler(404)
 def not_found():
-    return make_response("Not found" 404)
+    return make_response("Not found", 404)
+
+app.run()
