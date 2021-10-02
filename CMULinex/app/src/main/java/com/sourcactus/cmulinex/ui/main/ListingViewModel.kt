81c91c7fd891356ab59a7bbc1165e926b34c5d7d
Ryan Kim <2021rkim@tjhsst.edu>
2021-10-02 17:58:03 -0400

    Format time better

--- CMULinex/app/src/main/java/com/sourcactus/cmulinex/ui/main/ListingViewModel.kt
+++ CMULinex/app/src/main/java/com/sourcactus/cmulinex/ui/main/ListingViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import java.lang.Math.floor
+import java.lang.Math.round
 
 class ListingViewModel : ViewModel() {
 
@@ -16,7 +18,7 @@ class ListingViewModel : ViewModel() {
     }
 
     val nameInfo: LiveData<String> = Transformations.map(_name) {
-        "$it (${_index.value}): \n ${_time.value} minutes"
+        "$it (${_index.value}): \n ${round(floor(_time.value!!))} minutes,  ${round(60 * (_time.value!! - floor(_time.value!!)))} seconds"
     }
 
     fun setIndex(index: Int) {
