f544367d3f53e8bf7437efcbb612563020f7cba3
mlang25 <89793126+mlang25@users.noreply.github.com>
2021-10-01 22:46:57 -0400

    added db write capabilities

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, make_response, request, jsonify
 
+import json
+
+from db import database
+
 app = Flask(__name__)
 
 
@@ -8,29 +12,32 @@ def home():
     return "hello"
 
 
-@app.route("/send", methods = ['GET'])
+@app.route("/send", methods=['GET'])
 def get_restaurant_info():
     if request.method != 'GET':
         return make_response('Malformed request', 400)
 
-    json_response = {i:i+1 for i in range(26)}
+    json_response = db.get_all()
     return json_response
 
 
-@app.route("/submit", methods = ['POST'])
+@app.route("/submit", methods=['POST'])
 def post_info():
     if request.method != 'POST':
         return make_response('Malformed request', 400)
 
-    data = request.form
+    data = request.json
+    db.insert_one(data)
     return make_response(
         "works",
         200
     )
-    
+
 
 @app.errorhandler(404)
 def not_found():
     return make_response("Not found", 404)
 
+
+db = database()
 app.run()
